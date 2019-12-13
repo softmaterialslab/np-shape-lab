76c9da720f170b0e99adec2298f38472c1a9d67d
Nicholas Brunk <38959843+nbrunk@users.noreply.github.com>
2019-12-13 01:42:51 -0500

    Adding more stripes.

--- src/interface.cpp
+++ src/interface.cpp
@@ -433,7 +433,7 @@ void INTERFACE::assign_random_q_values(double q_strength, double alpha, int num_
         for (; i < number_of_vertices * 5*(alpha / num_divisions); i++)
             V[permutations[i].second].q = q_strength * randomAreaList[i] / total_area;
         for (; i < number_of_vertices * 6*(alpha / num_divisions); i++)
-            V[permutations[i].second].q = q_strength * randomAreaList[i] / total_area;
+            V[permutations[i].second].q = 0;
         for (; i < number_of_vertices * 7*(alpha / num_divisions); i++)
             V[permutations[i].second].q = q_strength * randomAreaList[i] / total_area;
         for (; i < number_of_vertices; i++)
