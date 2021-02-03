cd276ac9ac0177ac7ecc7e122780b4a93828f3e7
fanbsun <fanbsun@iu.edu>
2021-02-03 15:15:12 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -189,7 +189,7 @@ int main(int argc, const char *argv[]) {
     notify(vm);
 
     // Compute the box radius (where NP radius is unit length so it is not found here):
-    if(counterionFlag != 'y') packing_fraction = 1.0;
+    //if(counterionFlag != 'y') packing_fraction = 1.0;
     box_radius = pow(1 / packing_fraction,1.0/3.0);
     // Reducing the counterion diameter:
     counterion_diameter = (counterion_diameter / unit_radius_sphere);
