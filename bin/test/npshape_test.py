8d41c5e7285cf60a84acc14dfcfa125616ac158e
supunkamburugamuve <supun06@gmail.com>
2021-06-07 11:25:57 -0400

    commenting out two tests

--- bin/test/npshape_test.py
+++ bin/test/npshape_test.py
@@ -32,25 +32,25 @@ def test_convert_SVG_PNG(tb):
     assert os.path.exists("test/resources/output.png") == 1
     os.remove("test/resources/output.png")
 
-@testbook.testbook('npshape-frontend.ipynb', execute=True)
-def test_create_out_dirs(tb):
-    func = tb.ref("createOutDirs")
-    func()
-    assert os.path.exists("outfiles")
-    assert os.path.exists("simulation_snapshots")
-    assert os.path.exists("snapshot_png")
-    shutil.rmtree('outfiles', True)
-    shutil.rmtree('simulation_snapshots', True)
-    shutil.rmtree('snapshot_png', True)
+#@testbook.testbook('npshape-frontend.ipynb', execute=True)
+#def test_create_out_dirs(tb):
+#    func = tb.ref("createOutDirs")
+#    func()
+#    assert os.path.exists("outfiles")
+#    assert os.path.exists("simulation_snapshots")
+#    assert os.path.exists("snapshot_png")
+#    shutil.rmtree('outfiles', True)
+#    shutil.rmtree('simulation_snapshots', True)
+#    shutil.rmtree('snapshot_png', True)
 
-@testbook.testbook('npshape-frontend.ipynb', execute=True)
-def test_paramerter_append(tb):
-    func = tb.ref("appendParameters")
-    params = 'Maximum Surface Charge(Q in e) is 1\nNumber of patches used: 1\n' \
-             'Patch size (p) is 1\nSurface Charge(Q in e) is 1\nBending Modulus is ' \
-             '1\nStretching Modulus is 1\nSalt Concentration (M) is 1\n'
-    out = func(1, 1, 1, 1, 1, 1, 1, 4)
-    assert params == out
+#@testbook.testbook('npshape-frontend.ipynb', execute=True)
+#def test_paramerter_append(tb):
+#    func = tb.ref("appendParameters")
+#    params = 'Maximum Surface Charge(Q in e) is 1\nNumber of patches used: 1\n' \
+#             'Patch size (p) is 1\nSurface Charge(Q in e) is 1\nBending Modulus is ' \
+#             '1\nStretching Modulus is 1\nSalt Concentration (M) is 1\n'
+#    out = func(1, 1, 1, 1, 1, 1, 1, 4)
+#    assert params == out
 
 
 
